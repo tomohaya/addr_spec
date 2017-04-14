e0b87e1b07af64e20e2b0a24b1f2a350dc0c80d0
Tomoki Hayashi <tomoki.hayashi@mixi.co.jp>
2017-04-14 15:51:43 +0900

    add input

--- addr_spec.rb
+++ addr_spec.rb
@@ -1,6 +1,18 @@
 require 'test/unit'
+require 'stringio'
 
 class ValidateAddr
+  
+  @@num_list
+
+  
+  def input
+    @@num_list = $stdin.readlines.map(&:chomp).map(&:to_f)
+  end
+  
+  def cut_by_at(val)
+    val.split("@")
+  end
 
 end
 
@@ -9,6 +21,16 @@ class TestValidateAddr < Test::Unit::TestCase
     
   def setup
     @obj = ValidateAddr.new
+  end
+  
+  def test_input
+    $stdin = StringIO.new("abc\n")
+    assert_equal 0, @obj.input[0]
+    $stdin = STDIN
+  end
+  
+  def test_cut_by_at
+    assert_equal "abc", @obj.cut_by_at("abc@aaa")[0]
   end
   
   
